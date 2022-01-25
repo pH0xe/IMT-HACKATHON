8d233ffd623accd8d647ec660c89b224e221198c
Julien Reig <julien.reig6@gmail.com>
2022-01-25 23:13:53 +0100

    calcul du temps, map interractive, calcul vitesse (cassé)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,12 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          Poussière d'étoile inc.
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
-
-<!--    <q-drawer-->
-<!--      v-model="leftDrawerOpen"-->
-<!--      show-if-above-->
-<!--      bordered-->
-<!--    >-->
-<!--      <q-list>-->
-<!--        <q-item-label-->
-<!--          header-->
-<!--        >-->
-<!--          Essential Links-->
-<!--        </q-item-label>-->
-
-<!--        <EssentialLink-->
-<!--          v-for="link in essentialLinks"-->
-<!--          :key="link.title"-->
-<!--          v-bind="link"-->
-<!--        />-->
-<!--      </q-list>-->
-<!--    </q-drawer>-->
 
     <q-page-container>
       <router-view />
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,39 @@
 <template>
-  <q-page class="flex flex-center">
-    <l-map style="height:80vh; width: 90%" :options="{attributionControl: false}" :center="center" :zoom="zoom" :max-zoom="zoom">
+  <q-page class="row items-center">
+
+    <l-map
+      class="col-11 q-mx-md"
+      style="height:90vh; flex-grow: 2; width: auto"
+      :options="{attributionControl: false}"
+      :center="center"
+      :zoom="zoom"
+      :max-zoom="zoom"
+    >
       <l-tile-layer :url="url"></l-tile-layer>
       <l-polyline
         v-for="(polyline, index) in polylines"
-        :lat-lngs="polyline.latlngs"
+        :lat-lngs="getLat(index)"
         :color="polyline.color"
         :key="index"
       ></l-polyline>
-      <l-marker v-for="(marker, index) in markers" :lat-lng="marker.pos" :key="index" :name="marker.name"></l-marker>
     </l-map>
+
+    <div class="flex flex-center column q-mx-md col-1">
+      <span>Temps:</span>
+      <span>{{ time/1000 }} secondes</span>
+      <q-slider
+        v-model="time"
+        :min="0"
+        :max="getNbMilli"
+        :step="1000"
+        vertical
+        reverse
+      ></q-slider>
+      <span>Vitesse:</span>
+      <span>{{ getVitesse() }} </span>
+    </div>
+
+
   </q-page>
 </template>
 
@@ -23,8 +47,7 @@ export default {
   components: {
     LMap,
     LPolyline,
-    LTileLayer,
-    LMarker
+    LTileLayer
   },
 
   props: {},
@@ -33,26 +56,68 @@ export default {
     polylines: [],
     center: [47.282167, -1.520539],
     zoom: 20,
-    markers: [
-      {
-        pos: [47.282155, -1.520595],
-        name: 'Constant'
-      },
-      {
-        pos: [47.282160008774675, -1.5205599927049434],
-        name: 'Julien'
-      },
-    ]
+    time: 0,
   }),
 
-  computed: {},
+  computed: {
+    getMin() {
+      return this.polylines[0]?.times[0]
+    },
 
-  methods: {},
+    getMax() {
+      let max = null;
+      this.polylines.forEach(p => {
+        if (max === null) {
+          max = p.times[p.times.length-1];
+        } else {
+          if (p.times[p.times.length-1] > max) {
+            max = p.times[p.times.length-1];
+          }
+        }
+      });
+      return max;
+    },
+
+    getNbSecond() {
+      return (this.getMax - this.getMin) / 1000;
+    },
+
+    getNbMilli() {
+      return (this.getMax - this.getMin);
+    },
+  },
+
+  methods: {
+    getLat(index) {
+      const poly = this.polylines[index];
+      const search = this.time + this.getMin;
+      const i = poly.times.filter(t => t <= search);
+      return poly.latlngs.slice(0, i.length);
+    },
+
+    getVitesse() {
+      if (this.polylines.length > 0) {
+        const poly = this.polylines[1];
+        const search = this.time + this.getMin;
+        const i = poly.times.findIndex(t => t === search);
+        console.log(poly.vitesse);
+        return poly.vitesse[i];
+      }
+      return 0;
+    }
+  },
 
   mounted() {
     this.$store.dispatch('gps/fetchPure').then(res => {
       this.polylines = res;
+      this.time = this.getNbMilli;
     })
   }
 };
 </script>
+
+<style scoped>
+.q-slider {
+  margin: 10px
+}
+</style>
